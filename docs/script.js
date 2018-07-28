cc338f605ad04f3eb703770c4eed0070f9fe9540
Unknown <anita.piechocka@interia.pl>
2018-07-28 20:23:32 +0200

    Added /docs folder

---
+++ docs/script.js
@@ -0,0 +1,20 @@
+
+	$(document).ready(function(){
+				$(window).bind('scroll',function(e){
+			   		parallaxScroll();
+			   	});
+			   	
+			   	
+			   	function parallaxScroll(){
+			   		var scrolledY = $(window).scrollTop();
+					$('.bgWrapper').css('background-position','center -'+((scrolledY*0.1))+'px');
+		
+			   	}
+
+			   	
+			});
+	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox({alwaysShowClose: true});
+});
+	 baguetteBox.run('.tz-gallery');
